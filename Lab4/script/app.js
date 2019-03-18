44bd342aa124caee67fd91cc0689310a9ea32b06
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-18 13:54:34 +0100

    api only gets called on after a specific amount of time declared in DATATIMEOUT

--- Lab4/script/app.js
+++ Lab4/script/app.js
@@ -7,9 +7,8 @@ class Weather {
     }
 
     initialize() {
-        //this.getMyLocation();
+        this.getMyLocation();
         //console.log(navigator);
-        let timmy = setInterval(this.getMyLocation(), 1000);
     }
 
     getMyLocation() {
@@ -27,116 +26,153 @@ class Weather {
 
     getWeather(lat, lng) {
         // AJAX CALL / XHR
+        // https://api.darksky.net/forecast/a0d7e30c611f0dc709e266404cf156db/37.8267,-122.4233?units=si
         let roundedTemp = this;
+
+
         let url = `http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY}/${lat},${lng}?units=si`;
+        let now = new Date();
+        let t = now.getTime();
 
-        function fetchDemo() {
-            return fetch(url)
-                .then(response => {
-                    return response.json();
-                })
-                .then(json => {
-                    let t = json.currently.time;
-                    let currTemp = json.currently.temperature;
-                    let temp = document.createElement("h1");
-                    roundedTemp = Math.round(currTemp);
-                    // roundedTemp = 49;
-                    // test responsiveness naar temperatuur verandering door de let roundedTemp hardcoded te wijzigen
-                    temp.innerHTML = roundedTemp;
-                    document.querySelector(".temperatuur").appendChild(temp);
-
-                    let oldTime = localStorage.getItem('weather-time');
-                    if (oldTime) {
-                        // we have data
-                        let intOldTime = parseInt(oldTime);
-
-                        if (oldTime + DATATIMEOUT < t) {
-                            localStorage.setItem('weather-time', t);
-                            let currTemps = JSON.stringify(currTemp);
-                            localStorage.setItem('current-temperature', currTemps);
-                            console.log("list updated");
-                        } else {
-                            // no update required
-                            console.log("list is up to date");
-                        }
-                    } else {
-                        localStorage.setItem('weather-time', t);
-                        let currTemps = JSON.stringify(currTemp);
-                        localStorage.setItem('current-temperature', currTemps);
-                        console.log("list created");
-                    }
-
-
-                    return roundedTemp;
-                });
-        }
-
-        fetchDemo().then(result => {
-            console.log(result + "test");
-
-            class Yoga {
-                constructor() {
-                    this.initialize();
-                }
-
-                initialize() {
-                    this.getYogaPose();
-                }
-
-                getYogaPose() {
-                    let url = `https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json`;
-                    fetch(url)
+        let oldtime = localStorage.getItem('yoga-time');
+        if (oldtime) {
+            // we have data
+            let intOldTime = parseInt(oldtime);
+            console.log(intOldTime + "int old time");
+            if ((intOldTime + DATATIMEOUT) < t) {
+                // de tijd nu is groter dan de opgeslagen tijd + de timeout
+                localStorage.setItem('yoga-time', t);
+                function fetchDemo() {
+                    return fetch(url)
                         .then(response => {
                             return response.json();
                         })
                         .then(json => {
-                            let now = new Date();
-                            let t = now.getTime();
-                            console.log(t + "tajm");
-                            let temp = document.createElement("div");
-                            result = result - 1;
-                            if (result < 0 || result > 47) {
-                                result = Math.floor(Math.random() * 49);
-                                // if temperature is below 0 or above 47 show a random pose
-                            }
-
-                            let img = json[result].img_url;
-                            let poseName = json[result].english_name;
-                            console.log(json);
-                            temp.innerHTML = `${poseName}<img src=${img} width="100px">`;
-                            /* temp.innerHTML=`<img src=`; */
-                            // array nummer invullen op basis van de temperatuur
-                            document.querySelector(".yoga").appendChild(temp);
-
-                            let oldTime = localStorage.getItem('yoga-time');
-                            if (oldTime) {
-                                if (oldTime + DATATIMEOUT < t) {
-                                    localStorage.setItem('yoga-time', t);
-                                    let imgs = JSON.stringify(img);
-                                    let poseNames = JSON.stringify(poseName);
-                                    localStorage.setItem('yoga-image', imgs);
-                                    localStorage.setItem('yoga-posename', poseNames);
-                                    console.log("Yoga list updated");
-                                } else {
-                                    console.log("Yoga list is up to date");
-                                }
-                            } else {
-                                localStorage.setItem('yoga-time', t);
-                                let imgs = JSON.stringify(img);
-                                let poseNames = JSON.stringify(poseName);
-                                localStorage.setItem('yoga-image', imgs);
-                                localStorage.setItem('yoga-posename', poseNames);
-                                console.log("Yoga list created");
-                            }
-
+                            let temp = document.createElement("h1");
+                            roundedTemp = Math.round(json.currently.temperature);
+                            // roundedTemp = 49;
+                            // test responsiveness naar temperatuur verandering door de let roundedTemp hardcoded te wijzigen
+                            temp.innerHTML = roundedTemp;
+                            document.querySelector(".temperatuur").appendChild(temp);
+        
+                            return roundedTemp;
                         });
                 }
+        
+                fetchDemo().then(result => {
+                    console.log(result + "test");
+        
+                    class Yoga {
+                        constructor() {
+                            this.initialize();
+                        }
+        
+                        initialize() {
+                            this.getYogaPose();
+                        }
+        
+                        getYogaPose() {
+                            let url = `https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json`;
+                            fetch(url)
+                                .then(response => {
+                                    return response.json();
+                                })
+                                .then(json => {
+                                    let temp = document.createElement("div");
+                                    result = result - 1;
+                                    if (result < 0 || result > 47) {
+                                        result = Math.floor(Math.random() * 49);
+                                        // if temperature is below 0 or above 47 show a random pose
+                                    }
+                                    let img = json[result].img_url;
+                                    let poseName = json[result].english_name;
+                                    console.log(json);
+                                    temp.innerHTML = `${poseName}<img src=${img} width="100px">`;
+                                    /* temp.innerHTML=`<img src=`; */
+                                    // array nummer invullen op basis van de temperatuur
+                                    document.querySelector(".yoga").appendChild(temp);
+        
+                                });
+                        }
+                    }
+                    let yogaApp = new Yoga();
+        
+        
+                });
+                console.log("localStorage updated");
+            } else {
+
+                // load local storage info
+                console.log("localStorage is up to date");
             }
-            let yogaApp = new Yoga();
+
+        } else {
+            // no data stored yet
+            localStorage.setItem('yoga-time', t);
+            function fetchDemo() {
+                return fetch(url)
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(json => {
+                        let temp = document.createElement("h1");
+                        roundedTemp = Math.round(json.currently.temperature);
+                        // roundedTemp = 49;
+                        // test responsiveness naar temperatuur verandering door de let roundedTemp hardcoded te wijzigen
+                        temp.innerHTML = roundedTemp;
+                        document.querySelector(".temperatuur").appendChild(temp);
+
+                        return roundedTemp;
+                    });
+            }
+
+            fetchDemo().then(result => {
+                console.log(result + "test");
+
+                class Yoga {
+                    constructor() {
+                        this.initialize();
+                    }
+
+                    initialize() {
+                        this.getYogaPose();
+                    }
+
+                    getYogaPose() {
+                        let url = `https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json`;
+                        fetch(url)
+                            .then(response => {
+                                return response.json();
+                            })
+                            .then(json => {
+                                let temp = document.createElement("div");
+                                result = result - 1;
+                                if (result < 0 || result > 47) {
+                                    result = Math.floor(Math.random() * 49);
+                                    // if temperature is below 0 or above 47 show a random pose
+                                }
+                                let img = json[result].img_url;
+                                let poseName = json[result].english_name;
+                                console.log(json);
+                                temp.innerHTML = `${poseName}<img src=${img} width="100px">`;
+                                /* temp.innerHTML=`<img src=`; */
+                                // array nummer invullen op basis van de temperatuur
+                                document.querySelector(".yoga").appendChild(temp);
+
+                            });
+                    }
+                }
+                let yogaApp = new Yoga();
 
 
-        });
+            });
+
+            console.log("localStorage created");
+        }
+
     }
+
+    
 }
 
 
@@ -145,4 +181,4 @@ class Weather {
 
 let weatherApp = new Weather('a0d7e30c611f0dc709e266404cf156db');
 // nu hebben we een soort plugin geschreven waarbij andere gebruikers het programma ook kunnen gebruiken
-const DATATIMEOUT = 60;
+const DATATIMEOUT = 6000;
