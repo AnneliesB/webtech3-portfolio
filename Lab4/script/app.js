b5332a8c137a9c5cd9e2974e2e853cf044d3d3b4
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-18 10:34:06 +0100

    if temperature is below 0 return a random pose

--- Lab4/script/app.js
+++ Lab4/script/app.js
@@ -28,56 +28,62 @@ class Weather {
         // AJAX CALL / XHR
         // https://api.darksky.net/forecast/a0d7e30c611f0dc709e266404cf156db/37.8267,-122.4233?units=si
         let roundedTemp = this;
-        
+
 
         let url = `http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY}/${lat},${lng}?units=si`;
 
         function fetchDemo() {
             return fetch(url)
-            .then(response =>{
-                return response.json();
-            })
-            .then(json=>{
-                let temp = document.createElement("h1");
-                roundedTemp = Math.round(json.currently.temperature);
-                // roundedTemp = 18; test responsiveness naar temperatuur verandering door de let roundedTemp hardcoded te wijzigen
-                temp.innerHTML = roundedTemp;
-                document.querySelector(".temperatuur").appendChild(temp);  
-                console.log(roundedTemp + " in de fetch");
-                
-                return roundedTemp;
-            });
+                .then(response => {
+                    return response.json();
+                })
+                .then(json => {
+                    let temp = document.createElement("h1");
+                    roundedTemp = Math.round(json.currently.temperature);
+                    //roundedTemp = -18; 
+                    // test responsiveness naar temperatuur verandering door de let roundedTemp hardcoded te wijzigen
+                    temp.innerHTML = roundedTemp;
+                    document.querySelector(".temperatuur").appendChild(temp);
+                    console.log(roundedTemp + " in de fetch");
+
+                    return roundedTemp;
+                });
         }
-    
-        fetchDemo().then(result=>{
+
+        fetchDemo().then(result => {
             console.log(result + "test");
 
             class Yoga {
-                constructor(){
+                constructor() {
                     this.initialize();
                 }
-            
-                initialize(){
+
+                initialize() {
                     this.getYogaPose();
                 }
-            
-                getYogaPose(){
+
+                getYogaPose() {
                     let url = `https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json`;
                     fetch(url)
-                    .then(response =>{
-                        return response.json();
-                    })
-                    .then(json=>{
-                        let temp = document.createElement("div");
-                        result = result-1;
-                        let img = json[result].img_url;
-                        let poseName= json[result].english_name;
-                        console.log(json);
-                        temp.innerHTML=`${poseName}<img src=${img} width="100px">`;
-                        /* temp.innerHTML=`<img src=`; */
-                        // array nummer invullen op basis van de temperatuur
-                        document.querySelector(".yoga").appendChild(temp);
-                    });
+                        .then(response => {
+                            return response.json();
+                        })
+                        .then(json => {
+                            let temp = document.createElement("div");
+                            result = result - 1;
+                            if (result <0) {
+                                result = Math.floor(Math.random() * 49);
+                                // if temperature is below 0 show a random pose
+                            } 
+                                let img = json[result].img_url;
+                                let poseName = json[result].english_name;
+                                console.log(json);
+                                temp.innerHTML = `${poseName}<img src=${img} width="100px">`;
+                                /* temp.innerHTML=`<img src=`; */
+                                // array nummer invullen op basis van de temperatuur
+                                document.querySelector(".yoga").appendChild(temp);
+                            
+                        });
                 }
             }
             let yogaApp = new Yoga();
