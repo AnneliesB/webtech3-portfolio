36b5eb0ea9ddfeb4424e943a01fc92ea89191b33
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-25 11:40:28 +0100

    change path to files in lab folder

--- Lab_NodeJS_basics/index.js
+++ Lab_NodeJS_basics/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-let {requestListener} = require('./callbackFile.js');
+let {requestListener} = require('Lab_NodeJS_basics/callbackFile.js');
 const PORT = process.env.PORT || 4001;
 
 const server = http.createServer(requestListener);
