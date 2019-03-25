ea9f7f28c646199f48ddd2b56792eaf07b2a4930
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-25 11:58:27 +0100

    path css changed

--- Lab_NodeJS_basics/callbackFile.js
+++ Lab_NodeJS_basics/callbackFile.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = {
   requestListener: (req, res) => {
-  fs.readFile('Lab_NodeJS_basics/myWebsite.html',  'utf-8', (err, data) => {
+  fs.readFile('Lab_NodeJS_basics/index.html',  'utf-8', (err, data) => {
     if (err){
       res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(`${err}`);
