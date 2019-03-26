7e46663df7d95e746b65599b3d71e6d47f2f7666
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-26 11:59:36 +0100

    database and question scripts added

--- Lab5/api/question.js
+++ Lab5/api/question.js
@@ -1,9 +1,23 @@
-const { validate } = require('Lab5/middleware');
-const MessagesController = require('Lab5/controllers/messages');
+const express = require('express');
+const db = require('./db/db');
 
-module.exports = (app) => {
-    app.route('/messages').post(
-        validate,
-        MessagesController.add,
-    );
-};
+// set up express app
+const app = express();
+
+// get data from db
+app.get('/api/question', (req, res) => {
+    res.status(200).send({
+      success: 'true',
+      message: 'question retrieved successfully',
+      question: db
+    })
+  });
+
+  const PORT = 4001;
+
+  app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+  });
+  
+
+
---
+++ Lab5/db/db.js
@@ -0,0 +1,8 @@
+const question =  [
+    {
+      intent: "mijn intentie",
+      details: "meer details"
+    }
+];
+
+export default question;
