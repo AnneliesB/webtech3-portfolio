85ae7bced86641e9c6b742bedb35bffdde7e09ce
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-26 13:29:27 +0100

    online port for api changed to website ip

--- Lab5/app/routes/note_routes.js
+++ Lab5/app/routes/note_routes.js
@@ -1,6 +1,6 @@
 // routes/note_routes.js
 module.exports = function(app, db) {
-    app.post('/notes', (req, res) => {
+    app.post('/questions', (req, res) => {
         // You'll create your note here.
         console.log(req.body)
         res.send('Hello')
