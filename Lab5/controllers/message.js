23097c6b876d7249b82a8f7e1639673925dadc62
Annelies Bellon <annelies_bellon@hotmail.com>
2019-03-30 11:58:42 +0100

    routing: routes index & features page + message.get

---
+++ Lab5/controllers/message.js
@@ -0,0 +1,14 @@
+let Message = require('../models/message');
+
+let get = (req, res, next) =>{
+    Message.find({}, (err, docs) => {
+        res.json({
+            "status": "success",
+            "data": {
+                "messages": docs
+            }
+        });
+    });
+};
+
+module.exports.get = get;
--- Lab5/routes/index.js
+++ Lab5/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+let messageController = require('../controllers/message');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Home'
+  });
 });
+
+router.get('/index.pug', function (req, res, next) {
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+router.get('/features.pug', function (req, res, next) {
+  res.render('features', {
+    title: 'features'
+  });
+});
+
+router.get('/messages', messageController.get);
 
 module.exports = router;
