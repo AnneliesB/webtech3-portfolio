8857f2ba79e3b553a25f3a0a30dac0ef3c43357d
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-25 09:38:48 +0200

    add use Cors to index

--- Lab5/routes/index.js
+++ Lab5/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var cors = require('cors');
 
+app.use(cors());
+
 let messageController = require('../controllers/message');
 
 /* GET home page. */
