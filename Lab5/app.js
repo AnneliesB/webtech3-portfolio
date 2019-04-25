bfc172336426de5596ad2637ccc7eb4bb8d14299
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-25 09:45:43 +0200

    add cors to app.js

--- Lab5/app.js
+++ Lab5/app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -29,7 +30,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(cors());
+
 
 // error handler
 app.use(function(err, req, res, next) {
