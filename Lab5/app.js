0e0d302a913d93b9404f7210cb8a4763ada481e8
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-25 10:00:37 +0200

    add basic cors to app.js

--- Lab5/app.js
+++ Lab5/app.js
@@ -1,14 +1,16 @@
 var createError = require('http-errors');
+var cors = require('cors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors());
 
 
 // view engine setup
